abe7bb304e3ee3d229a7a4dd63d91bcc88b665e1
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-20 18:59:10 +0700

    Initial commit

---
+++ app/src/main/java/com/ithirteeng/cinemaproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ithirteeng.cinemaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
