128dbc29ac72e6625b0c753550376e30df03661d
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-21 17:58:27 +0700

    refactoring

--- app/src/main/java/com/ithirteeng/cinemaproject/di/AppModule.kt
+++ app/src/main/java/com/ithirteeng/cinemaproject/di/AppModule.kt
@@ -2,11 +2,11 @@ package com.ithirteeng.cinemaproject.di
 
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
-import com.ithirteeng.cinemaproject.navigation.buildCicerone
+import com.ithirteeng.cinemaproject.navigation.createCicerone
 import org.koin.dsl.module
 
 val appModule = module {
-    single { buildCicerone() }
+    single { createCicerone() }
     single { get<Cicerone<Router>>().router }
     single { get<Cicerone<Router>>().getNavigatorHolder() }
 }
--- app/src/main/java/com/ithirteeng/cinemaproject/navigation/CiceroneBuilder.kt
+++ app/src/main/java/com/ithirteeng/cinemaproject/navigation/CiceroneBuilder.kt
@@ -3,5 +3,5 @@ package com.ithirteeng.cinemaproject.navigation
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 
-fun buildCicerone(): Cicerone<Router> =
+fun createCicerone(): Cicerone<Router> =
     Cicerone.create()
