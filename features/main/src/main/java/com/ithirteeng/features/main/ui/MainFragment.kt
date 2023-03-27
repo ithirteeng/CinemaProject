3229f51f6d88b4e72aedf5ad0abc882a77873d18
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-27 17:54:08 +0700

    implemented for you adapter

--- features/main/src/main/java/com/ithirteeng/features/main/ui/MainFragment.kt
+++ features/main/src/main/java/com/ithirteeng/features/main/ui/MainFragment.kt
@@ -12,6 +12,7 @@ import com.ithirteeng.errorhandler.presentation.ErrorHandler
 import com.ithirteeng.features.main.R
 import com.ithirteeng.features.main.databinding.FragmentMainBinding
 import com.ithirteeng.features.main.presentation.MainFragmentViewModel
+import com.ithirteeng.features.main.ui.adapter.ForYouMoviesAdapter
 import com.ithirteeng.features.main.ui.adapter.InTrendMoviesAdapter
 import com.ithirteeng.features.main.ui.adapter.NewMoviesAdapter
 import com.ithirteeng.features.main.ui.adapter.RecentViewedMoviesAdapter
@@ -45,7 +46,14 @@ class MainFragment : Fragment() {
 
     private val newMoviesAdapter by lazy {
         NewMoviesAdapter {
-            Toast.makeText(requireContext(), "play ${it.name}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), it.name, Toast.LENGTH_SHORT).show()
+            // TODO: navigateToMovieInfo
+        }
+    }
+
+    private val forYouMoviesAdapter by lazy {
+        ForYouMoviesAdapter {
+            Toast.makeText(requireContext(), it.name, Toast.LENGTH_SHORT).show()
             // TODO: navigateToMovieInfo
         }
     }
@@ -61,10 +69,12 @@ class MainFragment : Fragment() {
         onGettingInTrendMoviesList()
         onGettingRecentViewedMoviesList()
         onGettingNewMoviesList()
+        onGettingForYouMoviesList()
 
         binding.inTrendRecyclerView.adapter = inTrendAdapter
         binding.recentRecyclerView.adapter = recentViewedAdapter
         binding.newRecyclerView.adapter = newMoviesAdapter
+        binding.forYouRecyclerView.adapter = forYouMoviesAdapter
 
         return binding.root
     }
@@ -81,6 +91,22 @@ class MainFragment : Fragment() {
             if (it.isNotEmpty()) {
                 binding.inTrendRecyclerView.visibility = View.VISIBLE
                 binding.inTrendTextView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun onGettingForYouMoviesList() {
+        viewModel.makeGetForYouMoviesListRequest { handleErrors(it) }
+        binding.progressBar.visibility = View.VISIBLE
+        viewModel.getForYouMoviesLiveData().observe(this.viewLifecycleOwner) {
+
+            finishedRequests++
+            forYouMoviesAdapter.submitList(it)
+            handleProgressBarVisibility()
+
+            if (it.isNotEmpty()) {
+                binding.forYouRecyclerView.visibility = View.VISIBLE
+                binding.forYouTextView.visibility = View.VISIBLE
             }
         }
     }
@@ -117,7 +143,7 @@ class MainFragment : Fragment() {
     }
 
     private fun handleProgressBarVisibility() {
-        if (finishedRequests == 3) {
+        if (finishedRequests == 4) {
             binding.progressBar.visibility = View.GONE
         }
     }
