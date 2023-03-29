ced0043c2d48921e72b61703e9c12169bb7d1a55
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-29 13:24:07 +0700

    implemented adapters

--- features/movieinfo/src/main/java/com/ithirteeng/features/movieinfo/ui/adapter/CadresAdapter.kt
+++ features/movieinfo/src/main/java/com/ithirteeng/features/movieinfo/ui/adapter/CadresAdapter.kt
@@ -1,4 +1,59 @@
 package com.ithirteeng.features.movieinfo.ui.adapter
 
-class CadresAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ithirteeng.features.movieinfo.R
+import com.ithirteeng.features.movieinfo.databinding.CadreItemLayoutBinding
+import com.ithirteeng.shared.movies.entity.MovieEntity
+
+class CadresAdapter :
+    ListAdapter<MovieEntity, CadresAdapter.CadresViewHolder>(MovieDiffCallBack) {
+
+    inner class CadresViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = CadreItemLayoutBinding.bind(view)
+
+        private lateinit var movieEntity: MovieEntity
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        fun bind(movieEntity: MovieEntity) {
+            this.movieEntity = movieEntity
+            Glide
+                .with(binding.root)
+                .load(movieEntity.poster)
+                .placeholder(binding.root.context.getDrawable(com.ithirteeng.component.design.R.drawable.image_placeholder))
+                .error(binding.root.context.getDrawable(com.ithirteeng.component.design.R.drawable.image_placeholder))
+                .into(binding.posterImageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CadresViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.cadre_item_layout, parent, false)
+
+        return CadresViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CadresViewHolder, position: Int) {
+        val movieEntity = getItem(position)
+        holder.bind(movieEntity)
+    }
+
+
+}
+
+object MovieDiffCallBack : DiffUtil.ItemCallback<MovieEntity>() {
+    override fun areItemsTheSame(oldItem: MovieEntity, newItem: MovieEntity): Boolean =
+        oldItem.id == newItem.id
+
+
+    override fun areContentsTheSame(oldItem: MovieEntity, newItem: MovieEntity): Boolean =
+        oldItem == newItem
+
 }
--- features/movieinfo/src/main/java/com/ithirteeng/features/movieinfo/ui/adapter/EpisodesAdapter.kt
+++ features/movieinfo/src/main/java/com/ithirteeng/features/movieinfo/ui/adapter/EpisodesAdapter.kt
@@ -1,4 +1,68 @@
 package com.ithirteeng.features.movieinfo.ui.adapter
 
-class EpisodesAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ithirteeng.features.movieinfo.R
+import com.ithirteeng.features.movieinfo.databinding.EpisodeItemLayoutBinding
+import com.ithirteeng.shared.movies.entity.EpisodeEntity
+
+class EpisodesAdapter(private val onEpisodeClick: (episodeId: String) -> Unit) :
+    ListAdapter<EpisodeEntity, EpisodesAdapter.EpisodesViewHolder>(EpisodesEntityDiffCallback) {
+
+    inner class EpisodesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = EpisodeItemLayoutBinding.bind(view)
+
+        private lateinit var episodeEntity: EpisodeEntity
+
+        init {
+            binding.clickableView.setOnClickListener {
+                onEpisodeClick(episodeEntity.episodeId)
+            }
+        }
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        fun bind(episodeEntity: EpisodeEntity) {
+            this.episodeEntity = episodeEntity
+            binding.episodeNameTextView.text = episodeEntity.name
+            binding.episodeDescriptionTextView.text = episodeEntity.description
+            binding.episodeYearTextView.text = episodeEntity.year
+            Glide
+                .with(binding.root)
+                .load(this.episodeEntity.preview)
+                .placeholder(binding.root.context.getDrawable(com.ithirteeng.component.design.R.drawable.image_placeholder))
+                .error(binding.root.context.getDrawable(com.ithirteeng.component.design.R.drawable.image_placeholder))
+                .into(binding.posterImageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodesViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.episode_item_layout, parent, false)
+
+        return EpisodesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EpisodesViewHolder, position: Int) {
+        val episodeEntity = getItem(position)
+        holder.bind(episodeEntity)
+    }
+
+
+}
+
+object EpisodesEntityDiffCallback : DiffUtil.ItemCallback<EpisodeEntity>() {
+    override fun areItemsTheSame(oldItem: EpisodeEntity, newItem: EpisodeEntity): Boolean =
+        oldItem.episodeId == newItem.episodeId
+
+
+    override fun areContentsTheSame(oldItem: EpisodeEntity, newItem: EpisodeEntity): Boolean =
+        oldItem == newItem
+
 }
